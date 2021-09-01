85a572e6cec13758b408c33f6a2967a9b02cd688
Vladyslav <v_sukachev@ukr.net>
2021-09-01 22:12:35 +0300

    add-block-search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,107 @@
-import './sass/main.scss';
+import '../src/scss/style.scss';
+import '../src/scss/lightbox.scss';
+
+import apiService from '../src/js/apiService';
+import updatePhotosMarkup from '../src/js/updatePhotosMarkup';
+import refs from '../src/js/refs';
+import '../src/js/spin';
+import * as basicLightbox from 'basiclightbox'
+import notice from '../src/js/pnotify'
+
+refs.searchForm.addEventListener('submit', onInputSearch);
+refs.loadMoreBtn.addEventListener('click', fetchPhotos);
+
+function onInputSearch(event) {
+    event.preventDefault();
+    apiService.query = event.target.elements.query.value;
+    refs.gallery.innerHTML = '';
+    apiService.resetPage();
+    fetchPhotos();
+}
+
+function fetchPhotos() {
+    spinnerOn();
+    disableButton();
+
+    apiService
+        .fetchPhoto()
+        .then(({ hits, total }) => {
+            updatePhotosMarkup(hits);
+            switchBtn(total);
+
+            window.scrollTo({
+                top: document.documentElement.offsetHeight,
+                behavior: 'smooth',
+            });
+
+            refs.gallery.addEventListener('click', onOpenModal)
+        })
+        .catch(err => console.log(err))
+        .finally(() => {
+            spinnerOff();
+            enableButton();
+        });
+}
+
+function onOpenModal(event) {
+    if (event.target.nodeName !== 'IMG') return;
+    const src = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src=${src} width="800" height="600">`)
+    instance.show()
+}
+
+function switchBtn(total) {
+    const { perPage, page } = apiService;
+    if (((page - 1) * perPage) >= total) {
+        hideButton();
+        notify(total);
+    } else { showButton() }
+}
+
+function notify(total) {
+    if (total === 0) {
+        notice({
+            text: 'Ничего не найдено',
+        })
+    }
+    if (total > 0) {
+        notice({
+            text: 'Больше нет((',
+        })
+    }
+}
+
+function showButton() {
+    refs.loadMoreBtn.classList.remove("is-hidden");
+};
+
+function hideButton() {
+    refs.loadMoreBtn.classList.add("is-hidden");
+};
+
+function enableButton() {
+    refs.loadMoreBtn.removeAttribute("disabled");
+};
+
+function disableButton() {
+    refs.loadMoreBtn.setAttribute("disabled", 'true');
+
+};
+
+function spinnerOn() {
+    refs.spinner.classList.remove('is-hidden')
+
+}
+
+function spinnerOff() {
+    refs.spinner.classList.add('is-hidden')
+}
+
+let searchInput = document.querySelector('.input-search'); // поиск инпута в документе
+
+const regex = /[а-яА-ЯёЁ]/i; // регулярное виражение на запрет ввода русских букв
+
+searchInput.oninput = function () {
+    this.value = this.value.replace(regex, "");
+};
---
+++ src/js/apiService.js
@@ -0,0 +1,33 @@
+export default {
+    apiKey: '18667452-b6cf3b15ecb06490e1251bb0b',
+    searchQuery: '',
+    perPage: 12,
+    page: 1,
+
+    fetchPhoto() {
+        const url = `https://pixabay.com/api/?q=${this.query}&image_type=photo&orientation=horizontal&per_page=${this.perPage}&key=${this.apiKey}&page=${this.page}`;
+        return fetch(url)
+            .then(res => {
+                this.incrementPage();
+                return res.json();
+            })
+            .catch(err => console.log(err));
+    },
+
+    resetPage() {
+        this.page = 1;
+    },
+
+    incrementPage() {
+        this.page += 1;
+    },
+
+    get query() {
+        return this.searchQuery.trim();
+    },
+
+    set query(value) {
+        this.searchQuery = value;
+    },
+
+};
